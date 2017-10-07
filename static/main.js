16f7a1f388ebbb8e6a6d81037a156790b3d58904
Laurent Erignoux <lerignoux@gmail.com>
2017-10-08 01:46:45 +0800

    adding a template graph to display

--- static/main.js
+++ static/main.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  angular.module('TransversalLookupApp', ['ngMaterial', 'md.data.table'])
+  angular.module('TransversalLookupApp', ['ngMaterial', 'md.data.table', 'chart.js'])
 
   .controller('LookupController', ['$scope', '$log', '$http',
     function($scope, $log, $http) {
@@ -20,6 +20,16 @@
         "database": null,
         "node": null,
         "attributes": []
+      };
+
+      $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+      $scope.series = ['Series A', 'Series B'];
+      $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+      ];
+      $scope.onClick = function (points, evt) {
+        console.log(points, evt);
       };
 
       $http.post('/login').
